cb105464f32cc1c48d447c20d986c891706b9e66
Philip Jönsson <fillejoensson@hotmail.com>
2023-12-20 16:04:21 +0100

    Fixat att användare blir låst efter 3 misslyckade log in försök.

--- Admin Login.cs
+++ Admin Login.cs
@@ -13,24 +13,33 @@ namespace DotNetDynamosV2
     /// </summary>
     internal class AdminLogin : IAdminLogin
     {
+
+        private static Dictionary<string, int> loginAttemptsCount = new Dictionary<string, int>();
+
         public Admin Login()
         {
             AsciiHeadliner.PrintHeadliner();
             Admin loggedInAdmin = null;
-            int loginAttempts = 0;
-            int maxLoginAttempts = 3; // Assuming a maximum of 3 login attempts
+            int maxLoginAttempts = 3;
 
-            while (loginAttempts < maxLoginAttempts && loggedInAdmin == null)
+            while (loggedInAdmin == null)
             {
                 Console.WriteLine("Username:");
                 string enteredName = Console.ReadLine();
-                // Validate if the entered username exists in CustomerUsers dictionary
+
+                if (IsUserLockedOut(enteredName))
+                {
+                    Console.Clear();
+                    Console.WriteLine("This account is locked. Please contact support.");
+                    break;
+                }
+
                 if (DataManager.adminList.ContainsKey(enteredName))
                 {
                     Console.WriteLine("Password:");
                     string enteredPassword = Validator.GetHiddenInput();
-                    // Perform password validation here
-                    if (ValidateAdminPassword(enteredName, enteredPassword)) // Example password validation
+
+                    if (ValidateAdminPassword(enteredName, enteredPassword))
                     {
                         Console.Clear();
                         Console.WriteLine("Welcome, " + enteredName + "!");
@@ -41,8 +50,8 @@ namespace DotNetDynamosV2
                     {
                         Console.Clear();
                         AsciiHeadliner.PrintHeadliner();
-                        loginAttempts++;
-                        Console.WriteLine($"Incorrect password. You have {maxLoginAttempts - loginAttempts} attempts remaining.");
+                        IncrementLoginAttempts(enteredName);
+                        Console.WriteLine($"Incorrect password. You have {maxLoginAttempts - loginAttemptsCount[enteredName]} attempts remaining.");
                     }
                 }
                 else
@@ -53,33 +62,116 @@ namespace DotNetDynamosV2
                 }
             }
 
-            if (loggedInAdmin == null)
-            {
-                Console.WriteLine("Maximum login attempts reached. Please contact support.");
-            }
-
             return loggedInAdmin;
-
         }
 
-        // Method to validate admin password
         private bool ValidateAdminPassword(string enteredName, string enteredPassword)
         {
-            // Check if the userID exists in the dictionary
             if (DataManager.adminList.ContainsKey(enteredName))
             {
-                // Retrieve the stored password corresponding to the userID
                 Admin storedUser = DataManager.adminList[enteredName];
                 return enteredPassword == storedUser.PassWord;
             }
             else
             {
-                // UserID not found, handle the case (throw an exception, return false, etc.)
-                // For example:
                 throw new ArgumentException("User ID not found.");
+            }
+        }
 
+        private void IncrementLoginAttempts(string username)
+        {
+            if (!loginAttemptsCount.ContainsKey(username))
+            {
+                loginAttemptsCount[username] = 1;
+            }
+            else
+            {
+                loginAttemptsCount[username]++;
             }
 
+            if (loginAttemptsCount[username] >= 3)
+            {
+                LockOutUser(username);
+            }
+        }
+
+        private bool IsUserLockedOut(string username)
+        {
+            return loginAttemptsCount.ContainsKey(username) && loginAttemptsCount[username] >= 3;
+        }
+
+        private void LockOutUser(string username)
+        {
+            Console.WriteLine($"User {username} is locked out. Please contact support.");
         }
     }
+    //public Admin Login()
+    //{
+    //    AsciiHeadliner.PrintHeadliner();
+    //    Admin loggedInAdmin = null;
+    //    int loginAttempts = 0;
+    //    int maxLoginAttempts = 3; // Assuming a maximum of 3 login attempts
+
+    //    while (loginAttempts < maxLoginAttempts && loggedInAdmin == null)
+    //    {
+    //        Console.WriteLine("Username:");
+    //        string enteredName = Console.ReadLine();
+    //        // Validate if the entered username exists in CustomerUsers dictionary
+    //        if (DataManager.adminList.ContainsKey(enteredName))
+    //        {
+    //            Console.WriteLine("Password:");
+    //            string enteredPassword = Validator.GetHiddenInput();
+    //            // Perform password validation here
+    //            if (ValidateAdminPassword(enteredName, enteredPassword)) // Example password validation
+    //            {
+    //                Console.Clear();
+    //                Console.WriteLine("Welcome, " + enteredName + "!");
+    //                loggedInAdmin = DataManager.adminList[enteredName];
+    //                AdminManager.Menu(loggedInAdmin);
+    //            }
+    //            else
+    //            {
+    //                Console.Clear();
+    //                AsciiHeadliner.PrintHeadliner();
+    //                loginAttempts++;
+    //                Console.WriteLine($"Incorrect password. You have {maxLoginAttempts - loginAttempts} attempts remaining.");
+    //            }
+    //        }
+    //        else
+    //        {
+    //            Console.Clear();
+    //            AsciiHeadliner.PrintHeadliner();
+    //            Console.WriteLine("Username not found.");
+    //        }
+    //    }
+
+    //    if (loggedInAdmin == null)
+    //    {
+    //        Console.WriteLine("Maximum login attempts reached. Please contact support.");
+    //    }
+
+    //    return loggedInAdmin;
+
+    //}
+
+    //// Method to validate admin password
+    //private bool ValidateAdminPassword(string enteredName, string enteredPassword)
+    //{
+    //    // Check if the userID exists in the dictionary
+    //    if (DataManager.adminList.ContainsKey(enteredName))
+    //    {
+    //        // Retrieve the stored password corresponding to the userID
+    //        Admin storedUser = DataManager.adminList[enteredName];
+    //        return enteredPassword == storedUser.PassWord;
+    //    }
+    //    else
+    //    {
+    //        // UserID not found, handle the case (throw an exception, return false, etc.)
+    //        // For example:
+    //        throw new ArgumentException("User ID not found.");
+
+    //    }
+
+    //}
 }
+
