70380a4dce862d2e72808a19c4a5860452c24641
Nanyria <145432308+Nanyria@users.noreply.github.com>
2023-12-22 11:25:09 +0100

    Fixar customer meny

--- CustomerManager (Menu).cs
+++ CustomerManager (Menu).cs
@@ -22,52 +22,133 @@ namespace DotNetDynamosV2
                 try
                 {
                     Console.WriteLine("Customer Menu");
-                    Console.WriteLine("1. View account and balance");
-                    Console.WriteLine("2. Transfer money between accounts");
-                    Console.WriteLine("3. Transfer money to other Customer");
-                    Console.WriteLine("4. Open new account");
-                    Console.WriteLine("5. Loan");
-                    Console.WriteLine("6. Account history");
-                    Console.WriteLine("7. Logg out");
-                    Console.Write("Choose meny: ");
-                    int svar = Convert.ToInt32(Console.ReadLine());
-                    switch (svar)
+                    Console.WriteLine("1. Withdraw and deposit");
+                    Console.WriteLine("2. Account services and loans");
+                    Console.WriteLine("3. Transfer services");
+                    Console.WriteLine("4. Logg out");
+                    Console.Write("Enter menu choice: ");
+                    int choice1 = Convert.ToInt32(Console.ReadLine());
+                    switch (choice1)
                     {
                         case 1:
                             Console.Clear();
-                            ShowBalance.ShowAccount(loggedInCustomer);
+                            Console.WriteLine
+                                ("Withdraw and deposit:\n" +
+                                "1. Withdraw.\n" +
+                                "2. Deposit. \n" +
+                                "Press enter to return to menu."
+                                );
+                            int choice2 = Convert.ToInt32(Console.ReadLine());
+                            switch (choice2)
+                            {
+                                case 1:
+                                    Console.Clear();
+                                    TransferMoney.Withdraw(loggedInCustomer);
+                                    Console.ReadKey();
+                                    break;
+                                case 2:
+                                    Console.Clear();
+                                    TransferMoney.Deposit(loggedInCustomer);
+                                    Console.ReadKey();
+                                    break;
+                                case 3:
+                                    Console.Clear();
+                                    Console.ReadKey();
+                                    break;
+                                default:
+                                    Console.Clear();
+                                    Console.WriteLine("Insert number between 1-2.");
+                                    break;
+                            }
                             break;
                         case 2:
                             Console.Clear();
-                            TransferMoney.TransferMoneyBetweenAccount(loggedInCustomer);
-                            Console.ReadKey();
+                            Console.WriteLine
+                                ("Account services:\n" +
+                                "1. View accounts and balance.\n" +
+                                "2. Open new account. \n" +
+                                "3. Account history.\n\n" +
+                                "Press enter to return to menu."
+                                );
+                            int choice3 = Convert.ToInt32(Console.ReadLine());
+                            switch (choice3)
+                            {
+                                case 1:
+                                    Console.Clear();
+                                    ShowBalance.ShowAccount(loggedInCustomer);
+                                    Console.ReadKey();
+                                    break;
+                                case 2:
+                                    Console.Clear();
+                                    AccountManager.AddAccount(loggedInCustomer);
+                                    Console.ReadKey();
+                                    break;
+                                case 3:
+                                    Console.Clear();
+                                    Transaction.ShowTransactionHistory(loggedInCustomer);
+                                    Console.ReadKey();
+                                    break; 
+                                case 4:
+                                    Console.Clear();
+                                    LoanManager.RequestPersonalLoan(loggedInCustomer);
+                                    Console.ReadKey();
+                                    break;
+                                case 5:
+                                    Console.Clear();
+                                    Console.ReadKey();
+                                    break;
+                                default:
+                                    Console.Clear();
+                                    Console.WriteLine("Insert number between 1-3.");
+                                    break;
+                            }
                             break;
                         case 3:
                             Console.Clear();
-                            Console.WriteLine("Out of order.");
-                            Console.ReadKey();
+                            Console.WriteLine
+                                ("Transfer services:\n" +
+                                "1. Transfer between accounts.\n" +
+                                "2. Transfer to other person. \n" +
+                                "3. Show Transaction History.\n\n" +
+                                "Press enter to return to menu."
+                                );
+                            int choice4 = Convert.ToInt32(Console.ReadLine());
+                            switch (choice4)
+                            {
+                                case 1:
+                                    Console.Clear();
+                                    TransferMoney.TransferMoneyBetweenAccount(loggedInCustomer);
+                                    Console.ReadKey();
+                                    break;
+                                case 2:
+                                    Console.Clear();
+                                    TransferMoney.TransferMoeneyToOthers(loggedInCustomer);
+                                    Console.ReadKey();
+                                    break;
+                                case 3:
+                                    Console.Clear();
+                                    Transaction.ShowTransactionHistory(loggedInCustomer);
+                                    Console.ReadKey();
+                                    break;
+                                case 4:
+                                    Console.Clear();
+                                    Console.ReadKey();
+                                    break;
+                                default:
+                                    Console.Clear();
+                                    Console.WriteLine("Insert number between 1-3.");
+                                    break;
+                            }
+                            
                             break;
                         case 4:
                             Console.Clear();
-                            AccountManager.AddAccount(loggedInCustomer);
-                            Console.ReadKey();
-                            break;
-                        case 5:
-                            Console.Clear();
-                            LoanManager.RequestPersonalLoan(loggedInCustomer);
-                            break;
-                        case 6:
-                            Console.Clear();
-                            Transaction.ShowTransactionHistory(loggedInCustomer);
-                            break;
-                        case 7: // Account history
-                            Console.Clear();
-                            LogOut();
+                            LogOut(loggedInCustomer);
                             Console.ReadKey();
                             break;
                         default:
                             Console.Clear();
-                            Console.WriteLine("Insert number between 1-7.");
+                            Console.WriteLine("Insert number between 1-4.");
                             break;
                     }
                 }
@@ -79,9 +160,11 @@ namespace DotNetDynamosV2
 
             }
         }
-        private static void LogOut()
+        private static void LogOut(Customer loggedInCustomer)
         {
+            Console.Clear();
             Console.WriteLine("Logged out.");
+            loggedInCustomer = null;
             Starting_screen.StartProgram();
         }
        
